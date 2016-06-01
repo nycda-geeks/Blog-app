5d9f018ac472a787a03bd8e374d7dadf1d0d0505
Anthony <anthony_wever@yahoo.com>
2016-06-01 13:28:44 +0200

    added some jade files

--- src/app.js
+++ src/app.js
@@ -2,19 +2,20 @@ var Sequelize = require('sequelize');
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var pg = require('pg');
 
 var sequelize = new Sequelize('postgres', 'postgres', 12345, {
 	host: 'localhost',
 	dialect: 'postgres'
 });
 
-var User = this.sequelize.define('user' {
+var User = sequelize.define('user', {
 	name: Sequelize.STRING,
 	email: Sequelize.STRING,
 	password: Sequelize.STRING
 });
 
-var Post = sequelize.define('post' {
+var Post = sequelize.define('post', {
 	title: Sequelize.STRING,
 	body: Sequelize.TEXT
 });
@@ -25,9 +26,14 @@ Post.belongsTo(User);
 
 var app = express();
 
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
 app.use(session({
 	secret: 'nan nunka lo sa',
-	resave: true
+	resave: true,
+	saveUninitialized: false
 }));
 
 
@@ -46,11 +52,57 @@ app.get('/profile', function (request, response) {
 	if (user === undefined) {
 		response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile"));
 	} else {
-		respone.render('profile', {
+		response.render('profile', {
 			user: user
 		});
 	}
 });
+
+app.get('/users', function (request, response) {
+	User.findAll().then(function (users) {
+		users = users.map(function (userRow) {
+			var columns = userRow.dataValues;
+			return {
+				id: columns.id,
+				name: columns.name,
+				email: columns.email
+			}
+		});
+
+		response.render('users', {
+			users: users
+		});
+	});
+});
+
+app.get('/posts', function (request, response) {
+	Post.findAll().then(function (posts) {
+		posts = posts.map(function (postRow) {
+			var columns = postRow.dataValues;
+			return {
+				id: columns.id,
+				title: columns.title,
+				body: columns.body
+			}
+		});
+	});
+});
+app.get('/register', function (req, res){
+	res.render('register')
+});
+
+app.post('/register', function (req, res) {
+	sequelize.sync({force: true}).then(function() {
+		User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password
+		});
+	});
+	res.redirect('/')
+});
+
+
 
 app.post('/login', bodyParser.urlencoded({extended: true}), function (request, response) {
 	if(request.body.email.length === 0) {
@@ -63,12 +115,12 @@ app.post('/login', bodyParser.urlencoded({extended: true}), function (request, r
 		return;
 	}
 
-	User.fundOne({
+	User.findOne({
 		where: {
 			email: request.body.email
 		}
 	}).then(function (user) {
-		if (user !== null && request.body.passowrd === user.password) {
+		if (user !== null && request.body.password === user.password) {
 			request.session.user = user;
 			response.redirect('/profile');
 		} else {
@@ -78,6 +130,7 @@ app.post('/login', bodyParser.urlencoded({extended: true}), function (request, r
 		response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
 	});
 });
+
 
 app.get('/logout', function (request, response) {
 	request.session.destroy(function(error) {
@@ -90,8 +143,14 @@ app.get('/logout', function (request, response) {
 
 
 sequelize.sync({force: true}).then(function () {
-	var server = app.listen(3000, function() {
-		console.log('app listening on port: ' + server.address().port);
+	User.create({
+		name: "anthony",
+		email: "anthony_wever@yahoo.com",
+		password: "password"
+	}).then(function () {
+		var server = app.listen(3000, function() {
+			console.log('app listening on port: ' + server.address().port);
+		});
 	});
 }, function (error) {
 	console.log('sync failed: ');
